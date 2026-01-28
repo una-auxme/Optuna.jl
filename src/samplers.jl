5d6b02268604d1c47c18f414594b6f0c0890ea7d
Tobias Thummerer <tobias.thummerer@uni-a.de>
2026-01-28 16:00:31 +0100

    TPESampler initial

--- src/samplers.jl
+++ src/samplers.jl
@@ -19,9 +19,63 @@ An independent sampler that samples randomly
 struct RandomSampler <: BaseSampler
     sampler::Any
 
-    function RandomSampler(seed=nothing::Union{Nothing,UInt32})
+    function RandomSampler(seed::Union{Nothing,UInt32}=nothing)
         sampler = optuna.samplers.RandomSampler(
             isnothing(seed) ? PythonCall.pybuiltins.None : pyconvert(UInt32, seed)
+        )
+        return new(sampler)
+    end
+end
+
+"""
+    TPESampler(; ...)
+
+Sampler using TPE (Tree-structured Parzen Estimator) algorithm.
+For further information see the (Optuna documentation)[https://optuna.readthedocs.io/en/stable/reference/samplers/generated/optuna.samplers.TPESampler.html].
+
+## Keywords
+- `consider_prior::Bool` (default=true).
+- `prior_weight::Int32` (default=1.0).
+- `consider_magic_clip::Bool` (default=true).
+- `consider_endpoints::Bool` (default=false).
+- `n_startup_trials::Int32` (default=10).
+- `n_ei_candidates::Int32` (default=24).
+- `seed::Union{Nothing,UInt32}`: Seed for the random number generator (default=nothing).
+- `multivariate::Bool` (default=false).
+- `group::Bool` (default=false).
+- `warn_independent_sampling::Bool` (default=true).
+- `constant_liar::Bool` (default=false).
+"""
+struct TPESampler <: BaseSampler
+    sampler::Any
+
+    # ToDo: Add kwargs gamma=<function default_gamma>, weights=<function default_weights>, 
+    # ToDo: Add kwarf functions constraints_func=nothing, categorical_distance_func=nothing,
+    function TPESampler(;
+        consider_prior::Bool=true,
+        prior_weight::Int32=1.0,
+        consider_magic_clip::Bool=true,
+        consider_endpoints::Bool=false,
+        n_startup_trials::Int32=10,
+        n_ei_candidates::Int32=24,
+        seed::Union{Nothing,UInt32}=nothing,
+        multivariate::Bool=false,
+        group::Bool=false,
+        warn_independent_sampling::Bool=true,
+        constant_liar::Bool=false,
+    )
+        sampler = optuna.samplers.TPESampler(;
+            consider_prior=pyconvert(Bool, consider_prior),
+            prior_weight=pyconvert(Float64, prior_weight),
+            consider_magic_clip=pyconvert(Bool, consider_magic_clip),
+            consider_endpoints=pyconvert(Bool, consider_endpoints),
+            n_startup_trials=pyconvert(Int32, n_startup_trials),
+            n_ei_candidates=pyconvert(Int32, n_ei_candidates),
+            seed=isnothing(seed) ? PythonCall.pybuiltins.None : pyconvert(UInt32, seed),
+            multivariate=pyconvert(Bool, multivariate),
+            group=pyconvert(Bool, group),
+            warn_independent_sampling=pyconvert(Bool, warn_independent_sampling),
+            constant_liar=pyconvert(Bool, constant_liar),
         )
         return new(sampler)
     end
