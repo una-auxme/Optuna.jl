e0d619c0b9169cfed2957930e730f787b7b0af4b
Josef Kircher <Josef.Kircher@uni-a.de>
2026-02-11 12:26:00 +0100

    added test that wrapps pruner and sampler

--- src/study.jl
+++ src/study.jl
@@ -87,7 +87,7 @@ function load_study(
     pruner::Union{Nothing,BasePruner}=nothing,
 )
     study = optuna.load_study(;
-        study_name=study_name, storage=storage.storage, sampler=sampler, pruner=pruner
+        study_name=study_name, storage=storage.storage, sampler=isnothing(sampler) ? nothing : sampler.sampler, pruner=isnothing(pruner) ? nothing : pruner.pruner
     )
     return Study(study, artifact_store, storage)
 end
